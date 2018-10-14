a90c2822ca7bba2a9e4696f476d187e732c0be19
WSudul <wojciech.sudul@gmail.com>
2018-10-15 00:16:25 +0200

    Add data source map with get and post methods

--- parking-lot-detector-prototype.py
+++ parking-lot-detector-prototype.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
+
+sources = {}
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/status', methods=['GET'])
+def status():
+    return 'Current running sources: \n' + str(sources)
+
+
+@app.route('/source', methods=['GET', 'POST'])
+def manage_source():
+    if request.method == 'GET':
+        name = request.args.get('name')
+        if name in sources:
+            source_status = sources[name]
+        else:
+            source_status = 'unknown'
+        return 'Source status: ' + source_status
+    if request.method == 'POST':
+        name = request.form.get('name')
+        new_status = request.form.get('status')
+        sources[name] = new_status
+        return 'updated'
 
 
 if __name__ == '__main__':
