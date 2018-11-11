11227ec598d5bb8deb635547ba0bb01e56a3b5f3
WSudul <wojciech.sudul@gmail.com>
2018-11-11 21:59:10 +0100

    rework detector to be able to started from new process

--- detector/detector.py
+++ detector/detector.py
@@ -1,11 +1,16 @@
 import datetime
+from collections import namedtuple
+
 import cv2
 from openalpr import Alpr
 import sys
 
 VIDEO_SOURCE = 0  # default webcam address
+VIDEO_SOURCE_FILE = '../resources/videos/hispeed.mp4'
 WINDOW_NAME = 'openalpr'
 FRAME_SKIP = 12
+
+AlprConfiguration = namedtuple('AlprConfiguration', 'region, config_file, runtime_data_file, frame_skip')
 
 
 def video_source_properties(cap):
@@ -19,8 +24,11 @@ def video_source_properties(cap):
 
 class Detector:
 
-    def __init__(self, alpr_instance, video_source, frame_skip=FRAME_SKIP):
-        self.alpr_instance = alpr_instance
+    def __init__(self, config, video_source, frame_skip=FRAME_SKIP):
+        self.alpr_instance = Alpr(config.region, config.config_file, config.runtime_data_file)
+        if not self.alpr_instance.is_loaded():
+            print('Alpr instance could not be created')
+
         self.frame_skip = frame_skip
         self._video_source = video_source
         self._cap = cv2.VideoCapture(video_source)
@@ -39,17 +47,22 @@ class Detector:
         return data
 
     def run(self):
-        frame_number = 0
+        if self._running:
+            print('Detector is already running')
+            return False
+        else:
+            self._running = True
+
+        print(" self._cap.isOpened() =", self._cap.isOpened())
+
         if not self.is_working():
             print('Video capture not working.');
             return False
 
-        if self._running:
-            print('Detector is already running')
-            return False
-
+        frame_number = 0
         error_state = False
         try:
+            print('starting detector loop')
             while self._running:
                 last_read_status, frame = self._cap.read()
 
@@ -80,61 +93,40 @@ class Detector:
             print("Exception caught: ", e)
             error_state = True
         finally:
+            self.alpr_instance.unload()
             return not error_state;
 
     def stop(self):
         self._running = False
 
 
+def create_configuration():
+    alpr_configuration = AlprConfiguration('eu', '../resources/openalpr.conf', '../resources/runtime_data', FRAME_SKIP)
+    return alpr_configuration
+
+
+def run_detector(alpr_configuration, video_source):
+    detector = Detector(alpr_configuration, video_source)
+    print('is working : ', detector.is_working())
+
+    print(detector.video_source_properties())
+
+    return detector.run()
+
+
 def main():
-    alpr = Alpr('eu', '../resources/openalpr.conf', '../resources/runtime_data')
-    if not alpr.is_loaded():
-        print('Error loading OpenALPR')
-        sys.exit(1)
-    alpr.set_top_n(3)
-    # alpr.set_default_region('new')
-    cap = cv2.VideoCapture(VIDEO_SOURCE)
-    if not cap.isOpened():
-        alpr.unload()
-        sys.exit('Failed to open video file!')
+    alpr_configuration = create_configuration()
 
-    video_source_properties(cap)
-
-    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
-    cv2.setWindowTitle(WINDOW_NAME, 'OpenALPR video test')
-
-    _frame_number = 0
-    a = datetime.datetime.now()
-    while True:
-        ret_val, frame = cap.read()
-
-        if not ret_val:
-            print('VidepCapture.read() failed. Exiting...')
-            b = datetime.datetime.now()
-            c = b - a
-            print(c.total_seconds(), c.seconds, c.microseconds)
-            break
-
-        _frame_number += 1
-        if _frame_number % FRAME_SKIP != 0:
-            continue
-        cv2.imshow(WINDOW_NAME, frame)
-
-        ret, enc = cv2.imencode("*.bmp", frame)
-        results = alpr.recognize_array(bytes(bytearray(enc)))
-
-        # todo: use recognize_ndarray when updated to at least 2.3.1
-        # alpr.recognize_ndarray(frame)
-        for i, plate in enumerate(results['results']):
-            best_candidate = plate['candidates'][0]
-            print('Plate #{}: {:7s} ({:.2f}%)'.format(i, best_candidate['plate'].upper(), best_candidate['confidence']))
-
-        if cv2.waitKey(1) == 27:
-            break
-
-    cv2.destroyAllWindows()
-    cap.release()
-    alpr.unload()
+    from concurrent.futures import ProcessPoolExecutor
+    executor = ProcessPoolExecutor(max_workers=2)
+    print('submiting detector')
+    future_1 = executor.submit(run_detector, alpr_configuration, VIDEO_SOURCE)
+    future_2 = executor.submit(run_detector, alpr_configuration, VIDEO_SOURCE_FILE)
+    print('got future')
+    import time
+    time.sleep(1)
+    print(future_1.result(15))
+    print(future_2.result(15))
 
 
 if __name__ == "__main__":
