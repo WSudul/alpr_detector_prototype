8e1ae1844e1db6618f8035aae067903c852992d0
WSudul <wojciech.sudul@gmail.com>
2018-11-15 01:07:24 +0100

    Refractor Detector + rename to AlprDetector

--- detector/AlprDetector.py
+++ detector/AlprDetector.py
@@ -24,12 +24,13 @@ def video_source_properties(cap):
 
 class Detector:
 
-    def __init__(self, name, config, video_source, frame_skip=FRAME_SKIP):
+    def __init__(self, name, config, video_source, frame_skip=FRAME_SKIP, event_callback=None):
         self._name = name
         self.alpr_instance = Alpr(config.region, config.config_file, config.runtime_data_file)
         if not self.alpr_instance.is_loaded():
             print('Alpr instance could not be created')
         self.frame_skip = frame_skip
+        self.event_callback = event_callback
         self._video_source = video_source
         self._cap = cv2.VideoCapture(video_source)
         self._running = False
@@ -44,7 +45,20 @@ class Detector:
         data['width'] = self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         data['height'] = self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         data['codec'] = self._cap.get(cv2.CAP_PROP_FOURCC)
-        return data
+        return
+
+    @staticmethod
+    def _extract_results(alpr_result):
+        if alpr_result is None:
+            return None
+        if not alpr_result['results']:
+            return None
+        else:
+            result = []
+            for plate in alpr_result['results']:
+                for candidate in plate['candidates']:
+                    result.append([candidate['plate'], candidate['confidence']])
+            return result
 
     def run(self):
         if self._running:
@@ -60,10 +74,9 @@ class Detector:
         frame_number = 0
         error_state = False
         try:
-            print(self._name, ' starting detector loop')
+            print(self._name, ' starting detector loop for: ', self._video_source)
             while self._running:
                 last_read_status, frame = self._cap.read()
-
                 if not last_read_status:
                     print('Video capture.read() failed. Stopping the work')
                     self._running = False
@@ -71,19 +84,29 @@ class Detector:
                     break
 
                 frame_number += 1
-                if frame_number % self.frame_skip != 0:
+                if frame_number % self.frame_skip == 0:
                     frame_number = 0
                     continue
-
-                ret, enc = cv2.imencode("*.bmp", frame)
-                results = self.alpr.recognize_array(bytes(bytearray(enc)))
+                if cv2.waitKey(1) == 27:
+                    break
+                cv2.imshow(self._name, frame)
 
                 # todo: use recognize_ndarray when updated to at least 2.3.1
                 # alpr.recognize_ndarray(frame)
-                for i, plate in enumerate(results['results']):
-                    best_candidate = plate['candidates'][0]
-                    print(self._name, ' Plate #{}: {:7s} ({:.2f}%)'.format(i, best_candidate['plate'].upper(),
-                                                                           best_candidate['confidence']))
+                ret, enc = cv2.imencode("*.bmp", frame)
+                results = self.alpr_instance.recognize_array(bytes(bytearray(enc)))
+                if self.event_callback is not None:
+                    # send first recognized plate and all candidates
+                    extracted_results = self._extract_results(results)
+                    if extracted_results is None:
+                        continue
+
+                    callback_data = dict()
+                    # object is list of dict containing 'plate' and 'confidence'
+                    callback_data['candidates'] = extracted_results
+                    callback_data['detector'] = self._name
+                    self.event_callback(callback_data)
+
         except cv2.error as e:
             print("OpenCV Exception caught: ", e)
             error_state = True
@@ -92,6 +115,7 @@ class Detector:
             error_state = True
         finally:
             self.alpr_instance.unload()
+            self._running = False
             print(self._name, " is stopping")
             return not error_state
 
@@ -116,17 +140,7 @@ def run_detector(alpr_configuration, instance_name, video_source):
 def main():
     alpr_configuration = create_configuration()
 
-    from concurrent.futures import ProcessPoolExecutor
-    executor = ProcessPoolExecutor(max_workers=2)
-    print('submiting detector')
-    future_1 = executor.submit(run_detector, alpr_configuration, "detector_1", VIDEO_SOURCE)
-    future_2 = executor.submit(run_detector, alpr_configuration, "detector_2", VIDEO_SOURCE_FILE)
-    print('got future')
-    import time
-    time.sleep(1)
-    print(future_1.result(150))
-    print(future_2.result(150))
-
+    run_detector(alpr_configuration, "detector_2", VIDEO_SOURCE_FILE)
 
 if __name__ == "__main__":
     main()
