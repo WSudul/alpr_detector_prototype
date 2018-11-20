1e4f6671db6b560172a674205246eba51d09a334
WSudul <wojciech.sudul@gmail.com>
2018-11-20 01:33:07 +0100

    Fix incorrect member names in Server.py

--- ipc_communication/Server.py
+++ ipc_communication/Server.py
@@ -6,51 +6,52 @@ import zmq
 class Server:
     def __init__(self, address, port, message_handler):
         self._context = zmq.Context()
-        self._context.setsockopt(zmq.ZMQ_LINGER, 100)
-        self._socket = self._context.socket(zmq.REP)
+        # self._context.setsockopt(zmq.ZMQ_LINGER, 100)
+        self.__socket = self._context.socket(zmq.REP)
         self.__address = address
         self.__port = port
-        self._message_handler = message_handler
+        self.__message_handler = message_handler
 
     @classmethod
     def __create_full_address(cls, address, port):
         return address + ':' + str(port)
 
     def bind(self, address, port):
-        self._socket.bind(address, port)
+        self.__socket.bind(Server.__create_full_address(address, port))
 
     def unbind(self, address, port):
-        self._socket.unbind(Server.__create_full_address(address, port))
+        self.__socket.unbind(Server.__create_full_address(address, port))
 
     def receive_message(self):
         try:
-            message = self._socket.recv_json()
+            message = self.__socket.recv_json()
             if message is None:
                 print('Received message is None')
 
-            reply = self._message_handler(message)
-            self._socket.send_json(reply)
+            reply = self.__message_handler(message)
+            self.__socket.send_json(reply)
         except zmq.ZMQError as e:
             print("Server Exception caught: ", e)
 
 
 class AsyncServer(Server):
     def __init__(self, address, port, message_handler):
-        super().__init__(address, port, message_handler)
+        Server.__init__(self, address, port, message_handler)
         self.__run = False
+        self.__server_thread: Thread = None
 
     def __run_in_loop(self):
         while self.__run:
             self.receive_message()
 
     def run(self):
-        self.bind(self.__address, self.__port)
+        self.bind(self._Server__address, self._Server__port)
         if self.__run is not True:
             self.__run = True
-            server_thread = Thread(target=self.__run_in_loop)
-            server_thread.start()
-            server_thread.join()
+            self.__server_thread = Thread(target=self.__run_in_loop)
+            self.__server_thread.start()
 
     def stop(self):
-        self._socket.unbind()
+        self.__socket.unbind()
+        self.__server_thread.join()
         self.__run = False
