089e6662e428c6d167f0325b074de0ceee0ce5dc
WSudul <wojciech.sudul@gmail.com>
2018-11-22 18:28:04 +0100

    Refractor Server class to handle multiple sockets listening

--- ipc_communication/Server.py
+++ ipc_communication/Server.py
@@ -1,35 +1,74 @@
+from collections import namedtuple
 from threading import Thread
 
 import zmq
 
+AddressAndPort = namedtuple('AddressAndPort', 'address, port')
+FullAddressAndHandler = namedtuple('FullAddressAndHandler', 'address, callback')
+
 
 class Server:
-    def __init__(self, address, port, message_handler):
+    def __init__(self, address=None, port=None, message_handler=None):
         self._context = zmq.Context()
-        # self._context.setsockopt(zmq.ZMQ_LINGER, 100)
-        self.__socket = self._context.socket(zmq.REP)
-        self.__address = address
-        self.__port = port
-        self.__message_handler = message_handler
+        self.__sockets = dict()  # dict of {socket: {Address and Handler}}
+
+        self.__default_message_handler = message_handler
+        self.__poller = zmq.Poller()
+
+        if address is not None and port is not None:
+            self.bind(address, port)
 
     @classmethod
     def __create_full_address(cls, address, port):
         return address + ':' + str(port)
 
-    def bind(self, address, port):
-        self.__socket.bind(Server.__create_full_address(address, port))
+    def bind(self, address, port, handler=None):
+        full_address = Server.__create_full_address(address, port)
+
+        existing_addresses = list(i.address for i in self.__sockets.values())
+
+        if full_address in existing_addresses:
+            print('Socket was already bound to ', full_address)
+            return
+
+        socket = self._context.socket(zmq.REP)
+        socket.bind(Server.__create_full_address(address, port))
+
+        if handler is None:
+            handler = self.__default_message_handler
+
+        self.__sockets[socket] = FullAddressAndHandler(full_address, handler)
+        self.__poller.register(socket, zmq.POLLIN)
 
     def unbind(self, address, port):
-        self.__socket.unbind(Server.__create_full_address(address, port))
+        full_address = Server.__create_full_address(address, port)
+
+        address_socket_dict = {v.address: k for k, v in self.__sockets.items()}
+        socket = address_socket_dict.get(full_address, None)
+
+        popped_socket: zmq.Socket = self.__sockets.pop(socket, None)
+        if popped_socket is None:
+            print('No socket was bound to ', full_address)
+            return
+
+        popped_socket.unbind(Server.__create_full_address(address, port))
+        self.__poller.unregister(popped_socket)
+        self.__sockets.pop(full_address)
 
     def receive_message(self):
         try:
-            message = self.__socket.recv_json()
-            if message is None:
-                print('Received message is None')
+            events = dict(self.__poller.poll())
 
-            reply = self.__message_handler(message)
-            self.__socket.send_json(reply)
+            for event in events:
+                socket_data: FullAddressAndHandler = self.__sockets[event]
+
+                message = self.__sockets.recv_json()
+                if message is None:
+                    print('Received message is None')
+
+                reply = socket_data.callback(message)
+                self.__sockets.send_json(reply)
+
         except zmq.ZMQError as e:
             print("Server Exception caught: ", e)
 
@@ -45,13 +84,12 @@ class AsyncServer(Server):
             self.receive_message()
 
     def run(self):
-        self.bind(self._Server__address, self._Server__port)
         if self.__run is not True:
             self.__run = True
             self.__server_thread = Thread(target=self.__run_in_loop)
             self.__server_thread.start()
 
     def stop(self):
-        self.__socket.unbind()
+        self.__sockets.unbind()
         self.__server_thread.join()
         self.__run = False
