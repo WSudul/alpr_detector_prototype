9ced139673f66a77402a4839099994ab0f2055f7
WSudul <wojciech.sudul@gmail.com>
2019-05-25 22:00:14 +0200

    update forms and html display

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_babel import lazy_gettext as _l
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, InputRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from device.Device import DeviceStatus, DeviceLocation
 
@@ -50,23 +50,23 @@ def device_location_check(form, field):
 
 class DeviceForm(FlaskForm):
     name = StringField(_l('Name'), validators=[DataRequired()])
-    status = StringField(_l('Status'), validators=[DataRequired(), device_status_check])
+    status = SelectField(_l('Initial Status', choices=[('ON', 'ON'), ('OFF', 'OFF')]))
     video_source = StringField(_l('Video source'), validators=[DataRequired()])
     address = StringField(_l('Address'), validators=[DataRequired()])
     listener_port = IntegerField(_l('Listener Port'), validators=[DataRequired()])
-    location = StringField(_l('Location'), validators=[DataRequired(), device_location_check])
+    role = SelectField(_l('Role', choices=[('ENTRY', 'ENTRY'), ('EXIT', 'EXIT')]))
     capture = BooleanField(_l('Capture images'))
 
     submit = SubmitField(_l('Submit device creation'))
 
 
 class UpdateDeviceForm(FlaskForm):
-    name = StringField(_l('Device'), validators=[InputRequired()])
-    status = StringField(_l('Status'), validators=[device_status_check])
+    name = SelectField(_l('Device'))
+    status = SelectField(_l('New Status', choices=[('ON', 'ON'), ('OFF', 'OFF')]))
     video_source = StringField(_l('Video source'))
     address = StringField(_l('Address'))
     listener_port = IntegerField(_l('Listener Port'))
+    role = SelectField(_l('Role', choices=[('ENTRY', 'ENTRY'), ('EXIT', 'EXIT')]))
     capture = BooleanField(_l('Capture images'))
 
     submit = SubmitField(_l('Submit device update'))
-
